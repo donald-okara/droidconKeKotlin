3a11d33d04735ac6a2f6fc83f81fecc2badf285e
donald-okara <donaldokara123@gmail.com>
2025-12-23 23:01:01 +0300

    Refactor: Format code
    - Apply code formatting across presentation files
    - Remove unused imports

--- presentation/src/main/java/com/android254/presentation/sessionDetails/view/SessionDetailsScreen.kt
+++ presentation/src/main/java/com/android254/presentation/sessionDetails/view/SessionDetailsScreen.kt
@@ -66,7 +66,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.android254.presentation.models.SessionDetailsPresentationModel
